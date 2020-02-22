5c27290ecf8383045d09be9f662a71880e6113ed
Bogdan Cristal <bc1819@ic.ac.uk>
2020-02-22 00:32:11 +0000

    added tests for getVenues

--- src/tests/unit/testvenueobjects/testgetVenues.py
+++ src/tests/unit/testvenueobjects/testgetVenues.py
@@ -1,10 +1,24 @@
+from botocore.exceptions import ClientError
 from unittest import TestCase
-from unittest.mock import patch
-from main.venueobjects.getVenue import GetVenueHandler
-from main.lambdautils.DBConnection import DynamoConn
+from unittest.mock import MagicMock, Mock
+import json
+
+from main.venueobjects.getVenues import GetVenuesHandler
 
 
-class TestGetVenue(TestCase):
-    @patch('main.lambdautils.DBConnection.DynamoConn')
-    def setUp(self, mockConn) -> None:
-        self.handler = GetVenueHandler(mockConn())
+class TestGetVenuesHandler(TestCase):
+    def setUp(self) -> None:
+        self.testData = {'Items': [{'typeID': '1', 'priceRank': '1'}]}
+        self.dbObject = MagicMock()
+        self.dbObject.getTable().scan().__getitem__ = Mock(side_effect=self.testData['Items'])
+        self.handler = GetVenuesHandler(self.dbObject)
+        self.errorCase = {'statusCode': self.handler.clientErrorCode}
+        self.successCase = {'statusCode': self.handler.successCode,
+                            'body': json.dumps(self.testData['Items'][0], indent=4, cls=self.handler.encoder)}
+
+    def testExecutesNormalCase(self):
+        self.assertEqual(self.successCase, self.handler.handle_request(None, None))
+
+    def testReturnsErrorCodeWhenNoDataIsReturned(self):
+        self.dbObject.getTable().scan().__getitem__ = Mock(side_effect=[{}])
+        self.assertEqual(self.errorCase, self.handler.handle_request(None, None))

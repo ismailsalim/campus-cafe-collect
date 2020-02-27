ff4a0ab38b61cca924b7c37be298580c9fb79891
Bogdan Cristal <bc1819@ic.ac.uk>
2020-02-27 18:51:27 +0000

    added push order abstract method

--- src/main/posobjects/BasePOS.py
+++ src/main/posobjects/BasePOS.py
@@ -4,7 +4,11 @@ import abc
 class BasePOS(metaclass=abc.ABCMeta):
     def __init__(self, venueID: str):
         self.venueID = venueID
-
+        self.vat = 1.2
     @abc.abstractmethod
     def getMenu(self):
+        pass
+
+    @abc.abstractmethod
+    def pushOrder(self, order):
         pass
